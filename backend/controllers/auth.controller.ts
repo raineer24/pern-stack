cbf3d217849f38a8794bf39eab8d4c7bc0b61450
raineer <raineerdelarita@gmail.com>
2024-11-11 23:47:35 -0800

    generateToken

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,5 +1,56 @@
 import { Request, Response } from "express";
+import prisma from '../src/db/prisma.ts';
+import bcryptjs from 'bcryptjs';
 
-export const signup = async (req: Request, res: Response) => {}
+export const signup = async (req: Request, res: Response) => {
+    try {
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+
+        if(!fullName || !username || !password || !confirmPassword || !gender) {
+            return res.status(400).json({ error: "Please fill in all fields"});
+            }
+        if( password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match"});
+        }    
+
+        const user = await prisma.user.findUnique({ where: { username}});
+
+        if(user) {
+            return res.status(400).json({ error: "Username already exists"})
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        // https://avatar-placeholder.iran.liara.run/
+		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = await prisma.user.create({
+            data: {
+                fullName,
+                username,
+                password: hashedPassword,
+                gender,
+                profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+            }
+        });
+
+        if (newUser) {
+            // generate token in a sec
+            res.status(201).json({
+                id: newUser.id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data"});
+        }
+        
+    } catch (error) {
+        
+    }
+}
 export const login = async (req: Request, res: Response) => {}
 export const logout = async (req: Request, res: Response) => {}
