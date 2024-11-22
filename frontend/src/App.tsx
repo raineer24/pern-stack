adf5e246d2949f496e1b595015b90eda139f9273
raineer <raineerdelarita@gmail.com>
2024-11-21 18:54:44 -0800

    pages route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Login from "./_ui_design/pages/Login"
+import Home from "./_ui_design/pages/Home"
+import Signup from "./_ui_design/pages/Signup"
 
 
 function App() {
  
   return (
-    
-    <h1 className="text-red-500 text-6x1">
-    Hello world!
-  </h1>
+    <div className="p-4 flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
+   
     
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -5,5 +6,5 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [daisyui],
 };
