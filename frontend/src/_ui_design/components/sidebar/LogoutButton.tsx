123604ef98f7aa1a2f224fbb2ff8ccd02bfd8d17
raineer <raineerdelarita@gmail.com>
2024-11-26 16:02:28 -0800

    messageContainer

---
+++ frontend/src/_ui_design/components/sidebar/LogoutButton.tsx
@@ -0,0 +1,12 @@
+import { LogOut } from "lucide-react";
+import React from 'react';
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+        <LogOut className="w-6 h-6 text-white cursor-pointer"/>
+    </div>
+  )
+}
+
+export default LogoutButton
