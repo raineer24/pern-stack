0e97c827321a937d096ad629f7b0243271f91f29
raineer <raineerdelarita@gmail.com>
2024-11-27 02:27:16 -0800

    hooks / useSignup

--- frontend/src/_ui_design/components/GenderCheckbox.tsx
+++ frontend/src/_ui_design/components/GenderCheckbox.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-
-const GenderCheckbox = () => {
+const GenderCheckbox = ({onCheckboxChange, selectedGender}: {
+  selectedGender: string;
+  onCheckboxChange: (gender: "male" | "female") => void;
+}) => {
   return (
-    <div className='flex'>
-        <div className="form-control">
-        <label className="{label gap-2 cursor-pointer}">
-            <span className="label-text">Male</span>
-            <input type="checkbox" className="checkbox border-slate-900" />
+    <div className="flex">
+      <div className="form-control">
+        <label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
+          <span className="label-text">Male</span>
+          <input type="checkbox" className="checkbox border-slate-900" 
+          checked={selectedGender === "male"}
+          onChange={() => onCheckboxChange("male")}
+          />
         </label>
-        </div>
-        <div className="form-control">
-            <label className="{label gap-2 cursor-pointer}">
-                <span className="label-text">Female</span>
-                <input type="checkbox" className="checkbox border-slate-900" />
-            </label>
-        </div>
-      
+      </div>
+      <div className="form-control">
+        <label className={`label gap-2 cursor-pointer ${selectedGender === "female" ? "selected" : ""} `}>
+          <span className="label-text">Female</span>
+          <input type="checkbox" className="checkbox border-slate-900" 
+           checked={selectedGender === "female"}
+           onChange={() => onCheckboxChange("female")}
+          />
+        </label>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GenderCheckbox
+export default GenderCheckbox;
--- frontend/src/_ui_design/pages/Signup.tsx
+++ frontend/src/_ui_design/pages/Signup.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import GenderCheckbox from '../components/GenderCheckbox';
 
 const Signup = () => {
+    const [inputs, setInputs] = useState({
+        fullName: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+        gender: "",
+    });
+
+    const handleCheckboxChange = (gender: any) => {
+        setInputs({ ...inputs, gender});
+    };
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        console.log(inputs);
+    };
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-g bg-opacity-0">
@@ -10,38 +27,51 @@ const Signup = () => {
             Sign Up <span className='text-blue-500'> ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label className="label p-2">
                     <span className="text-base label-text">Full Name</span>
                 </label>
-                <input type="text" className="w-full input input-bordered h-10" />
+                <input type="text" className="w-full input input-bordered h-10" 
+                value={inputs.fullName}
+                onChange={(e) => setInputs({ ...inputs, fullName: e.target.value})}
+                />
             </div>
 
             <div>
                  <label className="label p-2">
                     <span className="text-base label-text">Username</span>
                  </label>
-                 <input type="text" placeholder='johndoe' className="w-full input input-bordered h-10" />
+                 <input type="text" placeholder='johndoe' className="w-full input input-bordered h-10" 
+                 value={inputs.username}
+                 onChange={(e) => setInputs({ ...inputs, username: e.target.value})}
+                 />
             </div>
 
             <div>
                 <div className="label">
                     <span className="text-base label-text">Password</span>
                 </div>
-                <input type="password" placeholder='Enter Password' className="w-full input input-bordered h-10" />
+                <input type="password" placeholder='Enter Password' className="w-full input input-bordered h-10" 
+                value={inputs.password}
+                onChange={(e) => setInputs({ ...inputs, password: e.target.value})}
+                />
             </div>
 
             <div>
                 <div className="label">
                     <span className="text-base label-text"> Confirm Password</span>
                 </div>
-                <input type="password" placeholder='Confirm Password' className="w-full input input-bordered h-10" />
+                <input type="password" placeholder='Confirm Password' className="w-full input input-bordered h-10"
+                value={inputs.confirmPassword}
+                onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value})}
+                />
             </div>
 
-           <GenderCheckbox />
-
-            <Link to='/login' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block" href='#'>
+           <GenderCheckbox selectedGender={inputs.gender} onCheckboxChange={handleCheckboxChange} />
+           
+           
+            <Link to='/login' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block" >
                 Already have an account?
             </Link>
 
