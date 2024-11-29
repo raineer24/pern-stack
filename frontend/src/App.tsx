463b4709f756525ca2fa179e26766e573870a040
raineer <raineerdelarita@gmail.com>
2024-11-29 02:17:12 -0800

    navigate to

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Login from "./_ui_design/pages/Login"
 import Home from "./_ui_design/pages/Home"
 import Signup from "./_ui_design/pages/Signup"
 import { Toaster } from "react-hot-toast"
+import { useAuthContext } from "./context/AuthContext"
 
 
 function App() {
+
+  const { authUser, isloading} = useAuthContext();
+
+  if(isloading) return null;
  
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={'/login'} />} />
+        <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to={'/'} />} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to={'/'} />} />
       </Routes>
       <Toaster />
     </div>
