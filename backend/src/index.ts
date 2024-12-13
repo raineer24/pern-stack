e8dcf2eb5de029950f64847ba827f1644c8b2939
raineer <raineerdelarita@gmail.com>
2024-12-12 17:48:48 -0800

    socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-const app = express();
-
+import path from "path";
 
 import authRoutes from "../routes/auth.route.js";
 import messageRoutes from "../routes/message.route.js";
 
 import dotenv from "dotenv";
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
 
 
 app.use(cookieParser()); // for parsing cookies
@@ -19,8 +20,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
 });
