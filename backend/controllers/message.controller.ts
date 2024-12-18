06c3ba7b4fd48e7c73d17633193008bf2c744362
raineer <raineerdelarita@gmail.com>
2024-12-17 23:40:34 -0800

    changes

--- backend/controllers/message.controller.ts
+++ backend/controllers/message.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../src/db/prisma.js";
+import { getReceiverSocketId, io } from "../src/socket/socket.js";
 
 export const sendMessage = async (req: Request, res: Response) => {
     try {
@@ -47,6 +48,12 @@ export const sendMessage = async (req: Request, res: Response) => {
                     }
                 }
             });
+        };
+
+        // SOCKET IO FUNCTIONALITY WILL GO HERE
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit('newMessage', newMessage);
         }
 
         res.status(201).json(newMessage);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,14 +11,25 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 
 
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json());
 
 
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV !== "development") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+	})
+}
 
 server.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
